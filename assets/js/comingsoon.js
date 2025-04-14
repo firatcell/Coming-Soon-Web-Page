c94afbcc9999fe67f730bd0b3b05c60899dac6c5
Firat <125474109+firatcell@users.noreply.github.com>
2025-04-14 13:45:26 +0100

    Update comingsoon.js

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,9 +1,17 @@
 const countdown = () => {
-    const countDate = new Date("Apr 14, 2025 13:37:00").getTime();
-
+    const countDate = new Date("2025-04-14T13:37:00").getTime();
     const currentTime = new Date().getTime();
-    console.log(currentTime);
     const gap = countDate - currentTime;
+
+    if (gap <= 0) {
+        document.querySelector('.day').innerText = 0;
+        document.querySelector('.hour').innerText = 0;
+        document.querySelector('.minutes').innerText = 0;
+        document.querySelector('.seconds').innerText = 0;
+        clearInterval(timerInterval); // stop the interval if needed
+        // Optionally, display a message like "We're live!" or "Time's up"
+        return;
+    }
 
     const millisecond = 1;
     const second = millisecond * 1000;
@@ -15,15 +23,11 @@ const countdown = () => {
     const textHour = Math.floor((gap % day) / hour);
     const textMinutes = Math.floor((gap % hour) / minutes);
     const textSecond = Math.floor((gap % minutes) / second);
-    const textMillisecond = Math.floor((gap % second) / millisecond);
 
-    console.log(textHour)
-    console.log(textMillisecond)
     document.querySelector('.day').innerText = textDay;
     document.querySelector('.hour').innerText = textHour;
     document.querySelector('.minutes').innerText = textMinutes;
     document.querySelector('.seconds').innerText = textSecond;
-
 };
 
-setInterval(countdown, 250);
+const timerInterval = setInterval(countdown, 250);
