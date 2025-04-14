20ad59060d027d93adf4eaf5b10759913c1065fd
Firat <125474109+firatcell@users.noreply.github.com>
2025-04-14 13:37:51 +0100

    Update comingsoon.js

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Dec 25, 2022 00:00:00").getTime();
+    const countDate = new Date("Apr 14, 2025 13:37:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
